c6c9d038c10caff0db25ac8540c642eaa728b672
Muiz Junaid Khan <mzkhan25@hotmail.com>
2019-05-17 14:40:37 +0500

    Delete functionality Implemented

--- Models/Models/Common/BaseException.cs
+++ Models/Models/Common/BaseException.cs
@@ -8,7 +8,7 @@ namespace Models.Models.Common
     public class BaseException
     {
         public int Id { get; set; }
-        public bool IsError { get; set; }
+        public bool HasError { get; set; }
         public bool IsWarning { get; set; }
         public string Message { get; set; }
     }
--- Server/Interaction/Repository/RecipeRepository.cs
+++ Server/Interaction/Repository/RecipeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using Contract;
@@ -43,7 +44,17 @@ namespace Interaction.Repository
             BaseException baseException = new BaseException();
             try
             {
-                throw new NotImplementedException();
+                var dbItem = _db.Recipes.FirstOrDefault(user => user.Id == id);
+                _db.Entry(dbItem).State = EntityState.Modified;
+                if (dbItem != null)
+                {
+                    dbItem.DateModified = DateTime.UtcNow;
+                    dbItem.IsDeleted = true;
+                    baseException.Id = dbItem.Id;
+                }
+                _db.SaveChanges();
+                baseException.HasError = true;
+
             }
             catch (Exception e)
             {
